83c103e01d87b88a0ec4b87a72f48658a7a47e6c
Tyler Sernett Johnson <tylersernettjohnson@gmail.com>
2022-07-04 00:48:13 -0400

    Add error handling

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,12 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      console.log(req.body);
+      const {text, locale} = req.body;
+      if (!locale || text == undefined) {
+        return res.json({error: "Required field(s) missing"})
+      } else if (text == "") {
+        return res.json({error: "No text to translate"})
+      }
     });
 };
